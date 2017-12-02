28dec503aa1b30a892c3ad1ce7187d50ef19df6f
APWJL@agfa <mark.dechamps@agfa.com>
2017-12-02 12:56:52 +0100

    day1 part 2

--- day1/src/NumericInput.kt
+++ day1/src/NumericInput.kt
@@ -1,19 +1,29 @@
+import java.util.stream.Collectors
 
-
-class NumericInput constructor(private val input:List<Int> ) {
+class NumericInput constructor(private val inputString:String ) {
     fun addEqual(): Int {
 
-        if(input.isEmpty())return 0;
+        val input = mutableList(inputString)
+        if(input.isEmpty())return 0
         var sum =0
-        var first = input[0]
+        val mid = input.size/2
 
         for(i in input.indices){
-            val next = if ((i + 1) == input.size) first else input.get(i + 1)
+            val indexToCompareWith = i + mid
+            val next = if (indexToCompareWith >= input.size) input[indexToCompareWith -input.size] else input[indexToCompareWith]
             val thisOne = input.get(i)
             if(next == thisOne)sum += thisOne
         }
-        return sum;
+        return sum
     }
 
+    private fun mutableList(input: String): List<Int> {
+        val intList = input.chars()
+                .mapToObj { i -> i.toChar() }
+                .map { i -> "" + i }
+                .map { i -> i.toInt() }
+                .collect(Collectors.toList())
+        return intList
+    }
 
 }
